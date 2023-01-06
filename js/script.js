e29f601ad7bc0d29bb1ffd17f8aaafef15da9494
ezechukwu nwokedi <israellambert96@gmail.com>
2023-01-06 20:21:10 +0000

    added js for manual ops

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(
+    function(){
+        var manualAuto = $('#manual-automatic'); // container for manual and automatic
+        var manual = $('#manual'); // manual button
+        var manualSearch = $('#services-manual');
+        var closeManual = $("#close")
+        // add click event for manual search
+        manual.on('click', function(){
+            manualAuto.attr('class','hide')
+            manualSearch.removeClass('hide')
+        })
+
+        // add click event for manual search
+        closeManual.on('click', function(){
+            manualSearch.attr('class','hide')
+            manualAuto.removeClass('hide')
+        })
+    }
+)
