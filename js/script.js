d96d80ca38107c81a6ebf64ec173f58550db6cf0
ezechukwu nwokedi <israellambert96@gmail.com>
2023-01-06 21:57:20 +0000

    changed add-remove dynamics

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@ $(document).ready(
         var manualAuto = $('#manual-automatic'); // container for manual and automatic
         var manual = $('#manual'); // manual button
         var manualSearch = $('#services-manual');
-        var closeManual = $("#close");
-        
+        var closeManual = $("#close"); // close button
+
         // add click event for manual search
         manual.on('click', function(){
-            manualAuto.attr('class','hide')
+            manualAuto.addClass('hide')
             manualSearch.removeClass('hide')
         })
 
         // add click event for manual search
         closeManual.on('click', function(){
-            manualSearch.attr('class','hide')
+            manualSearch.addClass('hide')
             manualAuto.removeClass('hide')
         })
     }
