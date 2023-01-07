cf903091eefcd324e495fbc383b1c5ecce0c5df4
GeorgiaM93 <105521641+GeorgiaM93@users.noreply.github.com>
2023-01-07 14:17:41 +0000

    background images transition

--- js/script.js
+++ js/script.js
@@ -1,20 +1,42 @@
 $(document).ready(
-    function(){
+    function () {
         var manualAuto = $('#manual-automatic'); // container for manual and automatic
         var manual = $('#manual'); // manual button
         var manualSearch = $('#services-manual');
         var closeManual = $("#close"); // close button
+        const imageTag = $('#image')
 
         // add click event for manual search
-        manual.on('click', function(){
+        manual.on('click', function () {
             manualAuto.addClass('hide')
             manualSearch.removeClass('hide')
         })
 
         // add click event for manual search
-        closeManual.on('click', function(){
+        closeManual.on('click', function () {
             manualSearch.addClass('hide')
             manualAuto.removeClass('hide')
         })
+
+        // image transitions in the background
+        function changeImage() {
+            const images = [
+                'url("/images/egypt.jpg")',
+                'url("/images/egypt1.jpg")',
+                'url("/images/italy2.jpg")',
+                'url("/images/kenya.jpg")',
+                'url("/images/maldives1.jpg")',
+                'url("/images/paris.jpg")',
+            ]
+
+            const bg = images[Math.floor(Math.random() * images.length)];
+            console.log(bg)
+            imageTag.css({
+                "background-image": bg,
+                "background-size": "cover"
+            });
+        }
+
+        setInterval(changeImage, 3000)
     }
 )
