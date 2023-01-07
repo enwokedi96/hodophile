1bbf862c6da334680691ceffe5bb45d8241d78c3
GeorgiaM93 <105521641+GeorgiaM93@users.noreply.github.com>
2023-01-07 20:26:45 +0000

    changes

--- js/script.js
+++ js/script.js
@@ -26,9 +26,13 @@ $(document).ready(
                 'url("./images/egypt.jpg")',
                 'url("./images/egypt1.jpg")',
                 'url("./images/italy2.jpg")',
-                'url("./images/kenya.jpg")',
                 'url("./images/maldives1.jpg")',
                 'url("./images/paris.jpg")',
+                'url("./images/italy-rome-colosseum.jpg")',
+                'url("./images/peru.jpg")',
+                'url("./images/masai-mara-kenya.jpg")',
+                'url("./images/statue-of-liberty-us.jpg")',
+
             ]
 
             const bg = images[Math.floor(Math.random() * images.length)];
@@ -43,22 +47,24 @@ $(document).ready(
         setInterval(changeImage, 3000);
 
         // set increment and decrement factor for rooms and adults fields
-        function changeCount(id, jTag){
+        function changeCount(id, jTag) {
             // increase count
             if (id.includes('increase')) {
                 $(`#${id}`).on("click", function () {
                     currentNumRooms = parseInt(currentNumRooms) + 1;
                     jTag.val(`${currentNumRooms}`);
-                })}
+                })
+            }
             // decrease count
-            else if (id.includes('decrease')){
-                $(`#${id}`).on("click", function(){        
-                    if (parseInt(currentNumRooms)>1){
+            else if (id.includes('decrease')) {
+                $(`#${id}`).on("click", function () {
+                    if (parseInt(currentNumRooms) > 1) {
                         currentNumRooms = parseInt(currentNumRooms) - 1;
-                        jTag.val(`${currentNumRooms}`);}
-                    else(console.log("Boi, you can't book lower than a room!!"))
+                        jTag.val(`${currentNumRooms}`);
                     }
                     else (console.log("Boi, you can't book lower than a room!!"))
+                }
+                    else(console.log("Boi, you can't book lower than a room!!"))
                 }
                 )
             }
